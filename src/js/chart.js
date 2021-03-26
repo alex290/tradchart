b1c29ae683524a90cf017f6e34430d72d3b4dea0
alex <alex@webgoal.ru>
2021-03-26 20:27:00 +0300

    add watchlist

---
+++ src/js/chart.js
@@ -0,0 +1,44 @@
+function saveSess() {
+    // console.log(listChartText.value);
+    sessionStorage.setItem("listChartSess", listChartText.value);
+
+    if (listChartText.value == null) {
+        new TradingView.widget({
+            "autosize": true,
+            "symbol": "BINANCE:BTCUSDT",
+            "interval": "60",
+            "timezone": "Etc/UTC",
+            "theme": "dark",
+            "style": "1",
+            "locale": "ru",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "hide_legend": true,
+            "hide_side_toolbar": false,
+            "allow_symbol_change": true,
+            "save_image": false,
+            "container_id": "chart"
+        });
+    } else {
+        let arrList = listChartText.value.split("\n");
+        new TradingView.widget({
+            "autosize": true,
+            "symbol": "BINANCE:BTCUSDT",
+            "interval": "60",
+            "timezone": "Etc/UTC",
+            "theme": "dark",
+            "style": "1",
+            "locale": "ru",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "hide_legend": true,
+            "hide_side_toolbar": false,
+            "allow_symbol_change": true,
+            "watchlist": arrList,
+            "save_image": false,
+            "container_id": "chart"
+        });
+
+    }
+    closeMenu();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,54 @@
-new TradingView.widget({
-    "autosize": true,
-    "symbol": "BINANCE:BTCUSDT",
-    "interval": "60",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "ru",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_legend": true,
-    "hide_side_toolbar": false,
-    "allow_symbol_change": true,
-    "save_image": false,
-    "container_id": "chart"
-});
+var listChartText = document.getElementById("listChart");
 
+var listChartSess = sessionStorage.getItem('listChartSess');
 
+if (listChartSess == null) {
+    new TradingView.widget({
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "ru",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_legend": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "save_image": false,
+        "container_id": "chart"
+    });
+} else {
+    let arrList = listChartSess.split("\n");
+    new TradingView.widget({
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "ru",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_legend": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "watchlist": arrList,
+        "save_image": false,
+        "container_id": "chart"
+    });
+
+}
 
 
 
 function openMenu() {
     let bgmenuPage = document.querySelector('.bgmenu-page');
+    if (listChartSess != null) {
+        listChartText.value = listChartSess;
+    }
+
     bgmenuPage.classList.add("show");
 }
 
@@ -54,22 +82,44 @@ function gridChart() {
     }
 
     for (let index = 1; index <= grid; index++) {
-        new TradingView.widget({
-            "autosize": true,
-            "symbol": "BINANCE:BTCUSDT",
-            "interval": "60",
-            "timezone": "Etc/UTC",
-            "theme": theme,
-            "style": "1",
-            "locale": "ru",
-            "toolbar_bg": "#f1f3f6",
-            "enable_publishing": false,
-            "hide_legend": true,
-            "hide_side_toolbar": false,
-            "allow_symbol_change": true,
-            "save_image": false,
-            "container_id": "chart" + index
-        });
+        if (listChartSess == null) {
+            new TradingView.widget({
+                "autosize": true,
+                "symbol": "BINANCE:BTCUSDT",
+                "interval": "60",
+                "timezone": "Etc/UTC",
+                "theme": theme,
+                "style": "1",
+                "locale": "ru",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "hide_legend": true,
+                "hide_side_toolbar": false,
+                "allow_symbol_change": true,
+                "save_image": false,
+                "container_id": "chart" + index
+            });
+        } else {
+            let arrList = listChartSess.split("\n");
+            new TradingView.widget({
+                "autosize": true,
+                "symbol": arrList[index - 1],
+                "interval": "60",
+                "timezone": "Etc/UTC",
+                "theme": "dark",
+                "style": "1",
+                "locale": "ru",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "hide_legend": true,
+                "hide_side_toolbar": false,
+                "allow_symbol_change": true,
+                "allow_symbol_change": true,
+                "watchlist": arrList,
+                "save_image": false,
+                "container_id": "chart" + index
+            });
+        }
     }
 
     closeMenu();
